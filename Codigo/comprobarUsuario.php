d16a595c2b67a281671c5912671c10c31e60bfe9
abrahamelfahsi <abrahamelfahsi@gmail.com>
2021-12-13 20:55:28 +0100

    Despligue Codigo

--- Codigo/comprobarUsuario.php
+++ Codigo/comprobarUsuario.php
@@ -11,54 +11,12 @@ $conexion=conectar(false);
    echo "Usuario: $usuario <br>";
    echo "Password: $password <br>";
    //Hacemos la consulta
-   $existeUsuario = consultaLogin($conexion,$usuario,$password);
-   $fila = mysqli_fetch_assoc($existeUsuario);
-       foreach($fila as $atributo=>$valor){
-           echo $atributo." : ".$valor." <br>";
-       }
    //Hacemos la consulta del usuario para saber si no se acuerda de la contraseña
    //Comprobamos si existe el usuario
    $existeSoloUsuario=consultaUsuario($conexion, $usuario);
-   if(mysqli_num_rows($existeUsuario)==1){
-       $fila = mysqli_fetch_assoc($existeUsuario);
-       foreach($fila as $atributo=>$valor){
-           echo $atributo." : ".$valor." <br>";
-       }
-       crearSesion($fila);
-       echo "Mostramos los datos de la sesión<br>";
-       /*
-       //VOy a mostrar la sesion
-       foreach($_SESSION as $indice=>$valor){
-           echo $indice."=".$valor." <br>";
-       }
-       */
-       header('Location: principal.php');
-       
-      
-   }else{
-       if(mysqli_num_rows($existeSoloUsuario)==1){
-           //echo "contraseña incorrecta";
-           $fila = mysqli_fetch_assoc($existeSoloUsuario);
-           /*
-           foreach($fila as $atributo=>$valor){
-               echo $atributo." : ".$valor." <br>";
-           }
-           */
-           crearSesion($fila);
-           $url ="recuperar_pass.php";
-           $texto= "recuperar la contraseña";
-           echo "<a href=$url>$texto</a><br>";
-           $url1 ="Login.html";
-           $texto1= "volver al login";
-           echo "<a href=$url1>$texto1</a>";
-           header('Location: recuperar_pass.php');
-       }else{
-           echo "<p> El usuario no existe </p>";
-           $url ="ingresar_usuario.php";
-           $texto= "Ingresar usuario";
-           echo "<a href=$url>$texto</a>";
-           header('Location: ingreso.php');
-       }
-       
+   $fila = mysqli_fetch_assoc($existeUsuario);
+   foreach($fila as $atributo=>$valor){
+       echo $atributo." : ".$valor." <br>";
    }
+  
 ?>
