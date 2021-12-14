0b5bfc923359b9f1b6e19d21ff2ad07dbe8ef180
abrahamelfahsi <abrahamelfahsi@gmail.com>
2021-12-14 17:12:33 +0100

    Despligue Codigo

--- Despliegue/Instalar.sh
+++ Despliegue/Instalar.sh
@@ -12,7 +12,7 @@ PASSDB="debianDB"
 HOST=$(hostname -I)
 WWW="/var/www/html/"
 # Nombre del fichero de los datos en el proyecto 
-DATOS="tiendacompraventa.sql"
+DATOS="tienda.sql"
 BBDD="tiendacompraventa"
 
 # Se toman los parámetros como USER y PASS de la BBDD
