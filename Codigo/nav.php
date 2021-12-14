911f2cd87c307a0e522ff1bd964481e68db7565f
abrahamelfahsi <abrahamelfahsi@gmail.com>
2021-12-14 19:31:37 +0100

    Despligue Codigo

--- Codigo/nav.php
+++ Codigo/nav.php
@@ -2,7 +2,7 @@
 session_start();
 ?>
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
 
     <a class="navbar-brand" href="#">Navbar scroll</a>
@@ -15,40 +15,45 @@ session_start();
           <a class="nav-link" href="secciones.php">Secciones</a>
         </li>
       </ul>
-    
+      <ul class="nav navbar-nav">
       <?php
           if(isset($_SESSION['usuario'])){
             if($_SESSION['Rol']=="admin"){
               ?>
-              <ul class="navbar-nav mr-auto">
-            <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-            </ul>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                  <?php echo  "Bienvenido ".$_SESSION['usuario']; ?>
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                  <li><a class="dropdown-item" href="mostrarProductosComprar.php">Cesta</a></li>
+                  <li><a class="dropdown-item" href="subirProducto.php">Subir producto</a></li>
+                  <li><a class="dropdown-item" href="mostrarProductosVenta.php">mostrarProductosVenta</a></li>
+                  <li><a class="dropdown-item" href="adminUsuario.php">Panel usuario</a></li>
+                  <li><a class="dropdown-item" href="adminProducto.php">Panel productos</a></li>
+                  <li><a class="dropdown-item" href="adminSeccion.php">Panel Seccion</a></li>
+                  <li><a class="dropdown-item" href="adminComentario.php">Panel Comentarios</a></li>
+                  <li><a class="dropdown-item" href="cerrarSesion.php">Cerrar Usuario</a></li>
+                  <li><a class="dropdown-item" href="eliminarUsuario.php">Eliminar Usuario</a></li>
+               
+                  </ul>
               <?php
             }else if($_SESSION['Rol']=="usuario"){
               ?>
-<ul class="navbar-nav mr-auto">
-            <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
-      </ul>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                  <?php echo  "Bienvenido ".$_SESSION['usuario']; ?>
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                  <li><a class="dropdown-item" href="mostrarProductosComprar.php">Cesta</a></li>
+                  <li><a class="dropdown-item" href="subirProducto.php">Subir producto</a></li>
+                  <li><a class="dropdown-item" href="modificarUsuario.php">Modificar usuario</a></li>
+                  <li><a class="dropdown-item" href="mostrarProductosVenta.php">mostrarProductosVenta</a></li>
+                  <li><a class="dropdown-item" href="eliminarUsuario.php">Eliminar Usuario</a></li>
+                  <li><a class="dropdown-item" href="cerrarSesion.php">Cerrar Usuario</a></li>
+                  <li><a class="dropdown-item" href="eliminarUsuario.php">Eliminar Usuario</a></li>
+                </ul>
+              
+              
               <?php
             }
        
